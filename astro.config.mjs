04c07905e780114864f846fa315dcf7d7a71d89e
Shair17 <jimmy_jhair17@hotmail.com>
2024-01-18 12:07:10 -0500

    configuración de netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,14 +2,15 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 // import node from "@astrojs/node";
 // import vercel from '@astrojs/vercel/serverless';
-import deno from '@astrojs/deno';
+// import deno from '@astrojs/deno';
+import netlify from "@astrojs/netlify";
 import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react()],
   output: 'server',
-  adapter: deno(),
+  adapter: netlify()
   // adapter: node({
   //   mode: "standalone"
   // })
