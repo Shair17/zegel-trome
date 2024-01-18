b8675f5e945e4c5c53304fa7567dc2914ac2b33f
Shair17 <jimmy_jhair17@hotmail.com>
2024-01-18 11:49:35 -0500

    configuración de vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 // import node from "@astrojs/node";
-import vercel from '@astrojs/vercel/static';
+import vercel from '@astrojs/vercel/serverless';
 import react from "@astrojs/react";
 
 // https://astro.build/config
