0a6945acd931e3a7130804be27eb2f9238fa814a
Shair17 <jimmy_jhair17@hotmail.com>
2024-01-21 01:00:50 -0500

    cambiar adaptador a node

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,17 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-// import node from "@astrojs/node";
+import node from "@astrojs/node";
 // import vercel from '@astrojs/vercel/serverless';
 // import deno from '@astrojs/deno';
-import netlify from "@astrojs/netlify";
+// import netlify from "@astrojs/netlify";
 import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react()],
-  output: 'server',
-  adapter: netlify()
-  // adapter: node({
-  //   mode: "standalone"
-  // })
+  output: "hybrid",
+  // adapter: netlify()
+  adapter: node({
+    mode: "standalone",
+  }),
 });
